67d8d88180b577fbc9550bd032e67cbb52ea4a49
Akantor47 <36771317+Akantor47@users.noreply.github.com>
2018-02-23 18:19:55 +0100

    Add files via upload

---
+++ define.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+gpio -g mode 2 out
+gpio -g mode 3 out
+gpio -g mode 4 out
+gpio -g mode 17 out
+gpio -g mode 12 out
+gpio -g mode 16 out
+gpio -g mode 20 out
+gpio -g mode 21 out
+
+exit 0
